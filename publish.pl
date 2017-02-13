040a739afc69d4af4d47c65b42fa6c94b2ba40b6
Cindy Wang (CindyLinz) <cindylinz@gmail.com>
2017-02-13 18:01:40 +0800

    update home url

--- publish.pl
+++ publish.pl
@@ -22,7 +22,7 @@ my $frame_html = <<'.';
 <meta property=og:description content='<!-- DESCRIPTION -->'>
 <meta property=og:image content=http://cindylinz.github.io/Article/me.jpg>
 <meta property=og:image:secure_url content=https://cindylinz.github.io/Article/me.jpg>
-<link rel=stylesheet type=text/css href=style.css></head><body><article class="frame"><div class="author"><!-- Beautiful Cindy --></div><div class="content"><!-- Cindy is beautiful --></div></article><center style="margin-top:1em"><a href="/">home</a></center><script>
+<link rel=stylesheet type=text/css href=style.css></head><body><article class="frame"><div class="author"><!-- Beautiful Cindy --></div><div class="content"><!-- Cindy is beautiful --></div></article><center style="margin-top:1em"><a href=".">home</a></center><script>
   (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
   (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
   m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
