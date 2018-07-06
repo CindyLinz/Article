9bb34ed1905f9e2a04cf42187fb58d1c1c6200eb
Cindy Wang (CindyLinz) <cindylinz@gmail.com>
2018-07-06 17:08:27 +0800

    publish.pl with index list in each article page

--- publish.pl
+++ publish.pl
@@ -32,7 +32,7 @@ js = d.createElement(s); js.id = id;
 js.src = "//connect.facebook.net/zh_TW/sdk.js#xfbml=1&version=v2.8&appId=1742520292651539";
 fjs.parentNode.insertBefore(js, fjs);
 }(document, 'script', 'facebook-jssdk'));</script>
-<article class="frame"><div class="author"><!-- Beautiful Cindy --></div><div class="content"><!-- Cindy is beautiful --></div></article><center style="margin-top:.6em"><div class="fb-comments" data-href="<!-- SELF URL -->" data-width="600" data-numposts="5"></div></center><center style="margin-top:1em"><a href=".">home</a></center><script>
+<article class="frame"><div class="author"><!-- Beautiful Cindy --></div><div class="content"><!-- Cindy is beautiful --></div></article><div style="margin-top:.6em;display:flex;justify-content:center"><div class="fb-comments" data-href="<!-- SELF URL -->" data-width="600" data-numposts="5"></div><!-- INDEX --></div><center style="margin-top:1em"><a href=".">home</a></center><script>
   (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
   (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
   m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
@@ -46,9 +46,11 @@ fjs.parentNode.insertBefore(js, fjs);
 my %out;
 
 my $index_md < io('index.md');
+my $index_html = markdown($index_md);
+my($index_list_html) = $index_html =~ m#(<ul>.*</ul>)#s;
 
 $out{index} = $frame_html
-    =~ s/<!-- Cindy is beautiful -->/markdown($index_md)/re
+    =~ s/<!-- Cindy is beautiful -->/$index_html/r
     =~ s/<!-- TITLE -->/CindyLinz 的文章/gr
     =~ s/<!-- DESCRIPTION -->/CindyLinz 的文章/r
     =~ s#<!-- SELF URL -->#https://cindylinz.github.io/Article/#r;
@@ -65,7 +67,8 @@ while( $index_md =~ /\[[^][]*\]\(([^()]*)\.html\)/g ) {
         =~ s/<!-- TITLE -->/$title/gr
         =~ s/<!-- DESCRIPTION -->/$description/r
         =~ s#<!-- Beautiful Cindy -->#"<img width=50 height=50 src=https://avatars0.githubusercontent.com/u/285660?s=50><div>CindyLinz<br>$date_cap</div>"#re
-        =~ s#<!-- SELF URL -->#https://cindylinz.github.io/Article/$name.html#r;
+        =~ s#<!-- SELF URL -->#https://cindylinz.github.io/Article/$name.html#r
+        =~ s#<!-- INDEX -->#$index_list_html#r;
 }
 
 sys('git checkout gh-pages');
